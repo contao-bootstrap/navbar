3e73c1bf488eb9536414e2a2b912c88efde6f88f
David Molineus <david.molineus@netzmacht.de>
2017-05-12 11:32:42 +0200

    Add support for the managed edition.

--- src/ContaoBootstrapNavbarBundle.php
+++ src/ContaoBootstrapNavbarBundle.php
@@ -1,15 +1,18 @@
 <?php
 
 /**
- * @package    contao-bootstrap
- * @author     David Molineus <david.molineus@netzmacht.de>
- * @copyright  2016 netzmacht David Molineus. All rights reserved.
- * @filesource
- *
+ * @package   contao-bootstrap
+ * @author    David Molineus <david.molineus@netzmacht.de>
+ * @license   LGPL 3+
+ * @copyright 2013-2017 netzmacht creative David Molineus
  */
 
 namespace ContaoBootstrap\Navbar;
 
+use Contao\CoreBundle\ContaoCoreBundle;
+use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
+use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
+use ContaoBootstrap\Core\ContaoBootstrapCoreBundle;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 /**
@@ -19,4 +22,14 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
  */
 class ContaoBootstrapNavbarBundle extends Bundle
 {
+    /**
+     * {@inheritdoc}
+     */
+    public function getBundles(ParserInterface $parser)
+    {
+        $bundleConfig = BundleConfig::create(ContaoBootstrapNavbarBundle::class)
+            ->setLoadAfter([ContaoCoreBundle::class, ContaoBootstrapCoreBundle::class]);
+
+        return [$bundleConfig];
+    }
 }
---
+++ src/ContaoManager/Plugin.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * @package    Website
+ * @author     David Molineus <david.molineus@netzmacht.de>
+ * @copyright  2017 netzmacht David Molineus. All rights reserved.
+ * @filesource
+ *
+ */
+
+namespace ContaoBootstrap\Navbar\ContaoManager;
+
+use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
+use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
+
+/**
+ * Class Plugin
+ *
+ * @package ContaoBootstrap\Navbar\ContaoManager
+ */
+class Plugin implements BundlePluginInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getBundles(ParserInterface $parser)
+    {
+
+    }
+}
